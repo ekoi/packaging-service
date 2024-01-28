b1502f877772c14f4203f831a40537b0ff404849
ekoi <eko.indarto@dans.knaw.nl>
2024-01-28 15:36:51 +0100

    intermediate commit: modules for swh

--- src/modules/swh_api_depositor.py
+++ src/modules/swh_api_depositor.py
@@ -4,10 +4,12 @@ from time import sleep
 
 import json
 
+import jmespath
 import requests
 
 from src.bridge import Bridge
-from src.models.bridge_output_model import BridgeOutputDataModel, TargetResponse, ResponseContentType
+from src.models.bridge_output_model import BridgeOutputDataModel, TargetResponse, ResponseContentType, IdentifierItem, \
+    IdentifierProtocol
 from src.commons import logger, settings
 from src.dbz import DepositStatus
 
@@ -15,21 +17,27 @@ from src.dbz import DepositStatus
 class SwhApiDepositor(Bridge):
 
     def deposit(self) -> BridgeOutputDataModel:
-        target_resp = TargetResponse()
-        bridge_output_model = BridgeOutputDataModel(response= target_resp)
+        logger(f'DEPOSIT to {self.target.repo_name}', 'debug', self.app_name)
+        target_response = TargetResponse()
+        target_swh = jmespath.search("metadata[*].fields[?name=='repository_url'].value",
+                                     json.loads(self.metadata_rec.md))
+        bridge_output_model = BridgeOutputDataModel(response=target_response)
         headers = {'Content-Type': 'application/json', 'Authorization': f'Bearer {settings.SWH_ACCESS_TOKEN}'}
-        api_resp = requests.post(self.target.target_url, data="{}", headers=headers)
+        logger(f'self.target.target_url: {self.target.target_url}', 'debug', self.app_name)
+        url = f'{self.target.target_url}/{target_swh[0][0]}/'
+        api_resp = requests.post(url, data="{}", headers=headers)
+        logger(f'{api_resp.status_code} {api_resp.text}', 'debug', self.app_name)
         if api_resp.status_code == 200:
             api_resp_json = api_resp.json()
             logger(f'swh_api response json: {json.dumps(api_resp_json)}', 'debug', self.app_name)
             goto_sleep = False
             counter = 0 # TODO: Refactor using Tenancy!
-            while True or counter > settings.SWH_API_MAX_RETRIES:
+            while True and (counter < settings.SWH_API_MAX_RETRIES):
                 counter += 1
                 swh_check_url = api_resp_json.get("request_url")
                 check_resp = requests.get(swh_check_url, headers=headers)
                 if check_resp.status_code == 200:
-                    swh_resp_json = swh_check_url.json()
+                    swh_resp_json = check_resp.json()
                     logger(f'{swh_check_url} response: {json.dumps(swh_resp_json)}', 'debug', self.app_name)
                     if swh_resp_json.get('save_task_status') == DepositStatus.FAILED:
                         bridge_output_model.deposit_status = DepositStatus.FAILED
@@ -37,19 +45,29 @@ class SwhApiDepositor(Bridge):
                         break
                     elif swh_resp_json.get('snapshot_swhid'):
                         bridge_output_model.deposit_status = DepositStatus.FINISH
-                        target_resp.status_code = check_resp.status_code
-                        target_resp.content_type = ResponseContentType.JSON
-                        target_resp.content = json.dumps(swh_resp_json)
-                        target_resp.status = DepositStatus.SUCCESS
+                        target_response.status_code = check_resp.status_code
+                        target_response.content_type = ResponseContentType.JSON
+                        target_response.content = json.dumps(swh_resp_json)
+                        target_response.status = DepositStatus.SUCCESS
+                        identifier_items = []
+                        target_response.identifiers = identifier_items
+                        ideni = IdentifierItem(value=swh_resp_json.get('snapshot_swhid'), url=url,
+                                               protocol=IdentifierProtocol('swhid'))
+                        identifier_items.append(ideni)
                         break
                     else:
                         goto_sleep = True
                 if goto_sleep:
-                    sleep(settings.SWH_DELAY_POOLING)
+                    sleep(settings.SWH_DELAY_POLLING)
 
         else:
-            target_resp.status_code = api_resp.status_code
+            logger(f'ERROR api_resp.status_code: {api_resp.status_code}', 'debug', self.app_name)
+            target_response.status_code = api_resp.status_code
             bridge_output_model.deposit_status = DepositStatus.ERROR
-            target_resp.status_code = api_resp.status_code
+            target_response.error = json.dumps(api_resp.json())
+            target_response.content_type = ResponseContentType.JSON
+            target_response.status = DepositStatus.ERROR
+            target_response.content = json.dumps(api_resp.json())
+            logger(f'bridge_output_model: {bridge_output_model.model_dump(by_alias=True)}', 'debug', self.app_name)
 
         return bridge_output_model
--- src/modules/swh_sword_depositor.py
+++ src/modules/swh_sword_depositor.py
@@ -1,7 +1,16 @@
 from __future__ import annotations
 
+import json
+from datetime import datetime
+
+import requests
+from requests.auth import HTTPBasicAuth
+
 from src.bridge import Bridge
-from src.models.bridge_output_model import BridgeOutputDataModel
+from src.models.bridge_output_model import BridgeOutputDataModel, TargetResponse
+from src.commons import settings, DepositStatus, transform, logger, db_manager
+from time import sleep
+import sword2.deposit_receipt as dr
 
 
 class SwhSwordDepositor(Bridge):
@@ -9,8 +18,60 @@ class SwhSwordDepositor(Bridge):
     def deposit(self) -> BridgeOutputDataModel:
 
         bridge_output_model = BridgeOutputDataModel()
+        # create_sword_payload(self):
+        swh_form_md = json.loads(self.metadata_rec.md)
+        dv_target = db_manager.find_target_repo(self.dataset_id, self.target.input.from_target_name)
+        if dv_target:
+            swh_form_md.update({"doi": json.loads(dv_target.target_output)['response']['identifiers'][0]['value']})
+        logger(f"SwhSwordDepositor- swh_form_md - after update (doi): {json.dumps(swh_form_md)}", 'debug', self.app_name)
+        str_sword_payload = transform(
+            transformer_url=self.target.metadata.transformed_metadata[0].transformer_url,
+            str_tobe_transformed=json.dumps(swh_form_md)
+        )
+        logger(f'deposit to "{self.target.target_url}"', "debug", self.app_name)
+        headers = {
+            'Content-Type': 'application/atom+xml;type=entry',
+        }
+        auth = HTTPBasicAuth(settings.swh_sword_username, settings.swh_sword_password)
+        response = requests.post(self.target.target_url, headers=headers, auth=auth, data=str_sword_payload)
+        logger(f'status_code: {response.status_code}. Response: {response.text}', "debug", self.app_name)
+        if response.status_code == 200 or response.status_code == 201:  # TODO: remove 200, use only 201
+            rt = response.text
+            logger(f'sword response: {rt}', 'debug', self.app_name)
+            deposit_response = dr.Deposit_Receipt(xml_deposit_receipt=rt)
+            status_url = deposit_response.alternate
+            logger(f'Status request send to {status_url}', 'debug', self.app_name)
+            counter = 0
+            while True and (counter < settings.swh_api_max_retries):
+                counter += 1
+                sleep(settings.swh_delay_polling_sword)
+                rsp = requests.get(status_url, headers=headers, auth=auth)
+                if rsp.status_code == 200:
+                    rsp_text = rsp.text
+                    logger(f'response from {status_url} is {rsp_text}', 'debug', self.app_name)
+                    rsp_dep = dr.Deposit_Receipt(xml_deposit_receipt=rsp_text)
+                    print(rsp_dep.metadata)
+                    swh_metadata = rsp_dep.metadata
+                    swh_deposit_status = swh_metadata.get('atom_deposit_status')
+                    if swh_deposit_status and swh_deposit_status[0] == DepositStatus.DEPOSITED:
+                        target_repo = TargetResponse(url=self.target.target_url, status=DepositStatus.FINISH,
+                                                     message=rsp_text, content=rsp_text)
+                        target_repo.url = status_url
+                        target_repo.status_code = rsp.status_code
+                        target_repo.content = rsp_text
+                        bridge_output_model.deposit_status = DepositStatus.FINISH
+                        bridge_output_model.response = target_repo
+                        bridge_output_model.deposit_time = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S.%f")
+                        return bridge_output_model
 
+                else:
+                    raise ValueError(
+                        f'Error request to {status_url} with rsp.status_code: {rsp.status_code} and rsp.text: {rsp.text}')
+        else:
+            bridge_output_model.deposit_status = DepositStatus.ERROR
+            bridge_output_model.notes = response.text
         return bridge_output_model
+
 
 
 
